6e71c1edee575af26889a19f45575788c9c294c2
savanteke86 <41010966+savanteke86@users.noreply.github.com>
2018-07-09 20:00:07 +0530

    Fabinacci series

---
+++ savan.java
@@ -0,0 +1,24 @@
+package JavaProgram;
+
+import java.util.Scanner;
+
+public class fibona
+{
+	public static void main(String[] args) 
+	{
+		int n,a=0,b=0,c=1;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter nimber");
+		n=s.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			a=b;
+			b=c;
+			c=a+b;
+			System.out.println(a+"");
+			
+		}
+		
+	}
+
+}
